d2cedb8aa8b370e01995b6bb7e57fb658244ef2f
Titi24Pehuajo <tiagorodriguez58@gmail.com>
2023-04-26 19:25:50 -0300

    Avances

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,16 @@
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import SocialLinks from "./components/SocialLinks";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Home />
+
+      <SocialLinks />
+    </>
+  );
 }
 
 export default App;
--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-scroll";
 
 const NavBar = () => {
   const [nav, setNav] = useState(false);
@@ -26,16 +27,18 @@ const NavBar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
       <div>
-        <h1 className="text-3xl font-signature ml-2">Tiago</h1>
+        <h1 className="text-3xl ml-2 font-signature">Tiago</h1>
       </div>
 
       <ul className="hidden md:flex">
         {links.map(({ id, link }) => (
           <li
             key={id}
-            className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-110 duration-300"
+            className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-110 duration-300 "
           >
-            {link}
+            <Link to={link} smooth duration={500}>
+              {link}
+            </Link>
           </li>
         ))}
       </ul>
@@ -53,7 +56,16 @@ const NavBar = () => {
             <li
               key={id}
               className="px-4 cursor-pointer capitalize py-6 text-4xl"
-            ></li>
+            >
+              <Link
+                onClick={() => setNav(!nav)}
+                to={link}
+                smooth
+                duration={500}
+              >
+                {link}
+              </Link>
+            </li>
           ))}
         </ul>
       )}
