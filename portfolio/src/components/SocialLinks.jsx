286a80c72123fa0693209ed6444b544b290adb85
Titi24Pehuajo <tiagorodriguez58@gmail.com>
2023-06-21 17:06:53 -0300

    Cambio pequeño

--- portfolio/src/components/SocialLinks.jsx
+++ portfolio/src/components/SocialLinks.jsx
@@ -42,7 +42,7 @@ const SocialLinks = () => {
           <li
             key={id}
             className={
-              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-cyan-500" +
+              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-cyan-600" +
               " " +
               style
             }
